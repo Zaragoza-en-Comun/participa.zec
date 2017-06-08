212f9e04c9ab1fd09242a908f5949ec7433a8923
Hilfe <daniel.hilfe@gmail.com>
2017-06-08 12:51:49 +0200

    Set Passenger as server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,21 +6,23 @@ set :rvm_ruby_version, '2.3.3'
 set :branch, ENV['BRANCH'] || :master
 set :deploy_to, '/srv/rails/participa.zaragozaencomun.com'
 
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :start do
-    on roles(:app) do
-      execute "/etc/init.d/unicorn_production start"
-    end
-  end
-  task :stop do
-    on roles(:app) do
-      execute "/etc/init.d/unicorn_production stop"
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute "/etc/init.d/unicorn_production restart"
-    end
-  end
-end
+after 'deploy:publishing', 'passenger:restart'
+
+#after 'deploy:publishing', 'deploy:restart'
+#namespace :deploy do
+#  task :start do
+#    on roles(:app) do
+#      execute "/etc/init.d/unicorn_production start"
+#    end
+#  end
+#  task :stop do
+#    on roles(:app) do
+#      execute "/etc/init.d/unicorn_production stop"
+#    end
+#  end
+#  task :restart do
+#    on roles(:app) do
+#      execute "/etc/init.d/unicorn_production restart"
+#    end
+#  end
+#end
